af065030f0953dc1806486a90bdac7cc9ea53646
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-29 17:56:04 -0300

    learn(ap01-mss): encerra secao 4.3.43
    - retomar implementando o classificacoes

--- microsservicos/consulta/index.js
+++ microsservicos/consulta/index.js
@@ -49,6 +49,8 @@ app.post('/eventos', (req, res) => {
     // const type = evento.type
     // const payload = evento.payload
     const { type, payload } = evento
+    console.log(`type: ${type}`)
+    console.log(`payload: ${payload}`)
     //acessar o mapa de funções na posição type e chamar a função resultante entregando a ela, como parametro, o payload
     funcoes[type](payload)
   }
